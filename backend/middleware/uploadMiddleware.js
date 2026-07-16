e83b2bda4cdd391c4ea85c5695d7fdeeb0e6f834
shaad abbas <shaadabbas06@gmail.com>
2026-07-16 20:19:29 +0530

    fix: import syntax for multer-storage-cloudinary

--- backend/middleware/uploadMiddleware.js
+++ backend/middleware/uploadMiddleware.js
@@ -1,6 +1,7 @@
 import multer from "multer";
-import { CloudinaryStorage } from "multer-storage-cloudinary";
-import cloudinary from "../config/cloudinary.js";
+
+import pkg from 'multer-storage-cloudinary';
+const { CloudinaryStorage } = pkg;import cloudinary from "../config/cloudinary.js";
 
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
