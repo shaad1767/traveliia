e353d664e1c3546ec33e62c94b61294d2904a5db
shaad abbas <shaadabbas06@gmail.com>
2026-07-17 02:03:13 +0530

    fix: define isLocal in vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,22 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 
+// Check karein ki hum local development mein hain ya production (Vercel) par
+const isLocal = process.env.NODE_ENV === 'development';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),  tailwindcss()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       react: path.resolve('./node_modules/react'),
       'react-dom': path.resolve('./node_modules/react-dom')
-    } 
+    }
   },
   server: {
     proxy: {
       "/api": {
-          target: isLocal ? "http://localhost:5000" : "https://traveliia.onrender.com",        changeOrigin: true,
+        target: isLocal ? "http://localhost:5000" : "https://traveliia.onrender.com",
+        changeOrigin: true,
         secure: false
       }
     }
